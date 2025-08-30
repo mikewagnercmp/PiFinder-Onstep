1ac3cc8fadebce9377572c6c9e0bd6fdfaad5e1c
Mike Wagner <mi_go@hotmail.com>
2025-08-30 09:08:55 -0400

    Temporarily disable mount control imports to debug startup crash

--- python/PiFinder/__init__.py
+++ python/PiFinder/__init__.py
@@ -6,17 +6,17 @@ from . import utils
 from . import config
 from . import calc_utils
 
-# Import mount control modules
-from . import astro_physics_comm
-from . import mount_control_config
-from . import mount_control
+# Import mount control modules (temporarily disabled for debugging)
+# from . import astro_physics_comm
+# from . import mount_control_config
+# from . import mount_control
 
 # Make these available when importing from PiFinder
 __all__ = [
     'utils',
     'config',
     'calc_utils',
-    'astro_physics_comm',
-    'mount_control_config',
-    'mount_control'
+    # 'astro_physics_comm',
+    # 'mount_control_config',
+    # 'mount_control'
 ]
